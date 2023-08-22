32d86d0a2896ddac0357d83a69a1f879e47055b7
MarioA <maestradaz3@gmail.com>
2023-08-22 15:08:33 -0500

    Sin bd

--- cloroapp/settings.py
+++ cloroapp/settings.py
@@ -76,12 +76,12 @@ WSGI_APPLICATION = 'cloroapp.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
+"""DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
-}
+}"""
 
 
 # Password validation
