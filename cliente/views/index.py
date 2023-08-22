54957e67e69556231ef8d0b55554d8741d1e1059
MarioA <maestradaz3@gmail.com>
2023-08-22 18:44:10 -0500

    Varios

--- cliente/views/index.py
+++ cliente/views/index.py
@@ -2,10 +2,12 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from decouple import config
 
 def index(request):
     return HttpResponse("Documentacion Api RedeDoc")
     
 class SaludoView(APIView):
     def get(self, request):
-        return Response("Hola mundo")    
+        nombre = config('NOMBRE', default="Sin definir en variable de entorno")
+        return Response("Hola mundo " + nombre)    
