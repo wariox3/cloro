91be3380f212e8c6097c202a19bab04cb1fd894b
MarioA <maestradaz3@gmail.com>
2023-08-22 16:29:00 -0500

    Varios

--- cliente/views/index.py
+++ cliente/views/index.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
