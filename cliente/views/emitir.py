055c00dac526e9b81d6f71a984d648bc30e6d415
MarioA <maestradaz3@gmail.com>
2023-08-23 09:04:00 -0500

    Varios

--- cliente/views/emitir.py
+++ cliente/views/emitir.py
@@ -1,35 +1,26 @@
-from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status 
-import pymongo
-import os
+from pymongo import MongoClient
 from decouple import config
-#from cliente.serializer.documento import DocumentoSerializador
-#from cliente.models.documento import Documento
-#from azure.servicebus import ServiceBusClient, ServiceBusMessage
-#from decouple import config
+from azure.servicebus import ServiceBusClient, ServiceBusMessage
 import json
+import datetime
 
 class EmitirView(APIView):
     def post(self, request):  
-        client = pymongo.MongoClient(config('DATABASE_MONGO'))
-        dbname = client['cloro']
-        collection = dbname['documento']
-        mascot_1={
-            "name": "Sammy",
-            "type" : "Shark"
-        }
-        collection.insert_one(mascot_1)
-        return Response('Si señor')
-        """raw = request.data
-        documento = raw.get('documento')
-        documentoSerializador = DocumentoSerializador(data=documento)
-        if documentoSerializador.is_valid():
-            documentoMongo = documentoSerializador.save()            
-            with ServiceBusClient.from_connection_string(config('SERVICE_BUS_CONNECTION_STR')) as client:
-                with client.get_queue_sender(config('SERVICE_BUS_QUEUE_NAME')) as sender:
-                    single_message = ServiceBusMessage(json.dumps(documento))
-                    sender.send_messages(single_message)
-            return Response({"documento:": str(documentoMongo.pk)}, status=status.HTTP_200_OK)                
-        return Response({'error': documentoSerializador.errors}, status=status.HTTP_400_BAD_REQUEST)"""
+        raw = request.data
+        documentoMongo = raw
+        documentoMongo['fecha'] = datetime.datetime.now(tz=datetime.timezone.utc)
+        client = MongoClient(config('DATABASE_MONGO'))
+        db = client['cloro']
+        collection = db['documento']
+        id = collection.insert_one(documentoMongo).inserted_id 
+        del documentoMongo["_id"]        
+        documentoMongo["id"] = str(id)
+        documentoMongo['fecha'] = documentoMongo['fecha'].strftime("%Y-%m-%d %H:%M:%S")              
+        with ServiceBusClient.from_connection_string(config('SERVICE_BUS_CONNECTION_STR')) as client:
+            with client.get_queue_sender(config('SERVICE_BUS_QUEUE_NAME')) as sender:
+                single_message = ServiceBusMessage(json.dumps(documentoMongo))
+                sender.send_messages(single_message)
+        return Response({"documento:": str(id)}, status=status.HTTP_200_OK)                
